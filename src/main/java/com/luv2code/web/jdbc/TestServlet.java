ece489266520cfaf0ad3f182393695f8776bf119
Sergey Starikov <starikovs@ukr.net>
2018-01-14 11:41:31 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/web/jdbc/TestServlet.java
@@ -0,0 +1,43 @@
+package com.luv2code.web.jdbc;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Resource(name = "jdbc/web_student_tracker")
+    private DataSource dataSource;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        Connection myConn = null;
+        Statement meStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            myConn = dataSource.getConnection();
+            String sql = "select * from student";
+
+            myRs = meStmt.executeQuery(sql);
+
+            while(myRs.next()){
+                String email = myRs.getString("email");
+                out.println(email);
+            }
+        }catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+}
